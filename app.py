d49c764494a44e9a689625b0df834721ee94cf2d
= <=>
2024-01-19 14:20:55 -0300

    [ADD] Added landmark draw to function process_landmarks

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from utils import CvFpsCalc, init_video_capture_device, preprocess_frame, pressed_key, bounding_box_from_landmarks, exist_landmarks, draw_bounding_box
+from utils import *
 import mediapipe as mp 
 from configs import *
 
@@ -18,7 +18,8 @@ def process_landmarks(frame, landmarks):
     for hand_landmark, handedness in zip(landmarks.multi_hand_landmarks, landmarks.multi_handedness):
         hand_bounding_box = bounding_box_from_landmarks(frame, hand_landmark)
         frame = draw_bounding_box(frame, hand_bounding_box)
-        print(hand_bounding_box)
+        list_of_landmarks = convert_landmarks_to_list(frame, hand_landmark)
+        frame = draw_landmarks(frame, list_of_landmarks)
         cv2.imshow('frame', frame)
 
 def capture_webcam(cap_width, cap_height):
@@ -30,6 +31,7 @@ def capture_webcam(cap_width, cap_height):
         _, frame = webcam.read()
         frame = preprocess_frame(frame, cvFpsCalc=cvFpsCalc)
         landmarks = mp_hands_landmark_detector.process(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         if exist_landmarks(landmarks):
             process_landmarks(frame, landmarks=landmarks)
         else:
@@ -43,5 +45,5 @@ def capture_webcam(cap_width, cap_height):
 
 
 
-capture_webcam(600, 800)
+capture_webcam(frame_width, frame_height)
 
--- configs.py
+++ configs.py
@@ -1,4 +1,9 @@
+# Model Configs
 use_static_image_mode= True
 min_detection_confidence= 0.1
 min_tracking_confidence= 0.2
 num_hands = 1
+
+# Video Configs
+frame_width= 960
+frame_height= 540
